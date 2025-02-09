8c3b32f61b46bde21988b2fc682a123744444051
Rafal2228 <rafal2228@gmail.com>
2025-02-09 10:52:39 +0100

    Allow protected route request to be extended

--- backend/src/common/middleware/protectedRoute.ts
+++ backend/src/common/middleware/protectedRoute.ts
@@ -5,11 +5,11 @@ import { Address } from 'viem';
 import { verifyToken } from '@/api/auth/services/auth';
 import { ResponseStatus, ServiceResponse } from '@/common/models/serviceResponse';
 
-export type ProtectedRouteRequest = Request & {
+export type ProtectedRoute<R extends Request> = R & {
   user: { address: Address };
 };
 
-export const protectedRoute = () => (req: ProtectedRouteRequest, res: Response, next: NextFunction) => {
+export const protectedRoute = () => (req: ProtectedRoute<Request>, res: Response, next: NextFunction) => {
   try {
     const accessToken = req.headers.authorization?.split(' ')[1];
 
